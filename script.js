81be33c2809bb2109aa9e9e11f401c66d343da0f
swynnejr <saucersam@gmail.com>
2021-06-30 23:34:46 -0500

    F2C and C2F Temp changes with javascript

---
+++ script.js
@@ -0,0 +1,47 @@
+// Loading weather ALERTS
+
+function loadingB() {
+    alert("Loading weather report for Burbank, CA")
+}
+
+function loadingC() {
+    alert("Loading weather report for Chicago, IL")
+}
+
+function loadingD() {
+    alert("Loading weather report for Dallas, TX")
+}
+
+// Cookie footer delete on accept
+
+var cookieDiv = document.querySelector("#footer-div");
+
+function accept() {
+    cookieDiv.remove();
+}
+
+
+// Convert temp from C to F
+
+function convert(element) {
+    console.log(element.value);
+    for (let i = 1; i < 9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if (element.value == "°C") {
+            tempSpan.innerText = (f2c(tempVal));
+        } else {
+            tempSpan.innerText = (c2f(tempVal));
+
+        }
+        
+    }
+}
+
+function c2f(temp) {
+    return Math.round (9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+    return Math.round ((temp - 32) * 5 / 9);
+}
